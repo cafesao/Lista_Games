ef08e40d243bba3f1553011c48fdba1bbc391e28
cafesao <gabriel.diasdutra2@gmail.com>
2019-10-15 09:46:37 -0300

    [CS] Arquivo JS Principal

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
 import { PesquisarGame } from "./funcoes_Pesquisar"
 import { prepararObj } from "./funcoes_Adicionar"
 
+//PESQUISA
+
 let nomePesquisar = document.querySelector('input#nomePesquisarInput')
 
 let pesquisar = document.querySelector('button#pesquisar')
 pesquisar.addEventListener('click', Pesquisar)
+
+nomePesquisar.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) { 
+        Pesquisar()
+    }
+})
+
+function limparPesquisa() {
+    nomePesquisar.value = ''
+}
 
 function Pesquisar() {
     if(nomePesquisar.value.length == 0){
@@ -12,9 +24,11 @@ function Pesquisar() {
     }
     else {
         PesquisarGame(nomePesquisar.value)
+        limparPesquisa()
     }
 }
 
+//ADICIONAR
 
 let nome = document.querySelector('input#nomeInput')
 let tema = document.querySelector('input#temaInput')
@@ -26,11 +40,21 @@ let sobre = document.querySelector('#sobreInput')
 let adicionar = document.querySelector('button#adicionar')
 adicionar.addEventListener('click', Adicionar)
 
+function limparAdicionar() {
+    nome.value = ''
+    tema.value = ''
+    ano.value = ''
+    empresa.value = ''
+    engine.value = ''
+    sobre.value = ''
+}
+
 function Adicionar() {
     if(nome.value.length == 0 || tema.value.length == 0 || ano.value.length == 0 || empresa.value.length == 0 || engine.value.length == 0 || sobre.value.length == 0){
         alert('[ERRO] Por favor, insira TODOS os campos.')
     }
     else {
         prepararObj(nome.value, tema.value, ano.value, empresa.value, engine.value, sobre.value)
+        limparAdicionar()
     }
 }
