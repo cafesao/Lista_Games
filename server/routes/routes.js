e549535ece2653cf1122d8a1a71c1b75e2dc5c61
cafesao <gabriel.diasdutra2@gmail.com>
2019-10-15 09:54:02 -0300

    [CS] Arquivo de Rotas

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,13 @@
+//Constantes
 const express = require('express')
 const gamesControl = require('../controllers/gameController')
 const routes = express.Router()
 
+//Rotas
 routes.get('/dados', gamesControl.coletarTudo)
 routes.get('/dados/:nomeGame', gamesControl.coletar)
 routes.post('/dados', gamesControl.adicionar)
 routes.delete('/dados/:id', gamesControl.deletar)
 
+//Modulos
 module.exports = routes
